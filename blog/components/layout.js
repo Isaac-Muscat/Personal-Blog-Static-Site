3a277d8fe171a48cef911b883c6d7e3514d8f26b
Isaac Muscat <isaac.wieting.muscat@gmail.com>
2021-11-22 23:24:22 -0500

    Created new pages

--- blog/components/layout.js
+++ blog/components/layout.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
-import useRouter from "next/router";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
-  const header = <h1>This is a header</h1>;
-
+  const router = useRouter();
+  const header = (
+    <h1>
+      <Link href="/">Home</Link>
+      <Link href="/About">About</Link>
+      <Link href="/CodingExperiments">Coding Experiments</Link>
+    </h1>
+  );
   const footer = <h1>This is a footer</h1>;
 
   return (
---
+++ blog/pages/CodingExperiments.js
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function CodingExperiments() {
+  return (
+    <Layout>
+      <h1>Coding Experiements</h1>
+    </Layout>
+  );
+}
